d39dbb2cbd48117546c5324883457712e83f00fc
codermanu <manu.nevish@gmail.com>
2018-10-24 21:21:20 +0530

    1st commit
    1st commit

---
+++ contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<?php include("includes/header.php") ?>
+<h1 style="color: #2A24E2; background-color:#36EF67;"> Contact Us... <i class="glyphicon glyphicon-phone"></i></h1>
+<div>
+	<img src="img/ct.jpg" width="100%">
+</div>
+<div style="margin-left: 15px">
+<p style="font-size: 24px;"> If you're interested in any of the services we provide, we'd love to talk to you about the best way to solve your problems. Don't get late, register with us today. We'll contact you to follow up with further questions. </p>
+<p style="font-size: 16px"><b> N M H Savindra <br>
+	harinisavindra23@gmail.com<br>
+	0784242479 </b></p>
+</div>
+</body>
+</html> 
---
+++ homepage.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  </head>
+
+  <body background="images/back.jpg">
+      <?php include("includes/mdnload.php"); ?>
+    
+      <?php include("includes/header1.php"); ?>
+  
+
+
+
+  <h2>Smart Farming</h2>
+  
+  
+  
+       <div class="img.map">
+        
+  <img class="menuimg" src="images/pic.png" alt="menu" usemap="#menumap" hidefocus="true" align="center">
+  <map name="menumap">
+    <area shape="circle" coords="290,150,125" href="problems.php" title="I Have a Problem">
+    <area shape="circle" coords="750,180,125" href="sellharvest.php" title="Sell Your Harvest">
+    <area shape="circle" coords="180,550,125" href="watering.php" title="Water Your Farm">
+    <area shape="circle" coords="825,550,125" href="buyitems.php" title="Buy Items">
+
+</map>
+</div>
+
+</center>
+ 
+
+  
+
+      
+
+</body>
+
+  <?php include("includes/footer.php"); ?>
+</html>
